ed979c68c0485ce19fb4ae86ec0dc9f15547c7b3
Bhavesh <bhaveshjat9950@gmail.com>
2021-08-16 22:46:09 +0530

    sending function to / route

--- server.js
+++ server.js
@@ -26,7 +26,10 @@ today = dd + "-" + mm + "-" + yyyy;
 setInterval(() => mailNotifier("301427", today), 1740000);
 
 app.get("/", (req, res) => {
-    res.send("The is fucking running ###################### wooow");
+    res.send(
+        mailNotifier("301427", today),
+        "The is fucking running ###################### wooow"
+    );
 });
 // starting the serve
 app.listen(process.env.PORT, () => {
